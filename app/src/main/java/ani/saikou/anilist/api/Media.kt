9d7bcbb79002c51fd70c606e4cf70e99352bec2d
antonydp <38143733+antonydp@users.noreply.github.com>
2023-01-11 17:09:46 +0100

    ita

--- app/src/main/java/ani/saikou/anilist/api/Media.kt
+++ app/src/main/java/ani/saikou/anilist/api/Media.kt
@@ -203,14 +203,7 @@ enum class MediaStatus {
     FINISHED, RELEASING, NOT_YET_RELEASED, CANCELLED, HIATUS;
 
     override fun toString(): String {
-        return when(super.toString()){
-            "FINISHED" -> "FINITO"
-            "RELEASING" -> "IN CORSO"
-            "NOT_YET_RELEASED" -> "ANCORA NON RILASCIATO"
-            "CANCELLED" -> "CANCELLATO"
-            "HIATUS" -> "HIATUS"
-            else -> ""
-        }
+        return super.toString().replace("_", " ")
     }
 }
 
