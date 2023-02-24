31278b815d02c0c1b49f16bdfdb6ba206f81d421
antonydp <38143733+antonydp@users.noreply.github.com>
2023-02-24 20:18:02 +0100

    fixes

--- app/src/main/java/ani/saikou/parsers/AnimeSources.kt
+++ app/src/main/java/ani/saikou/parsers/AnimeSources.kt
@@ -18,15 +18,7 @@ object AnimeSources : WatchSources() {
 //        "INGLESE: 9Anime" to ::NineAnime,
         "INGLESE: AnimePahe" to ::AnimePahe,
         "INGLESE: ConsumeBili" to ::ConsumeBili,
-        "INGLESE: KickAssAnime" to ::KickAssAnime,
-        "SPAGNOLO: Animefenix" to ::Animefenix,
-        "SPAGNOLO: Animelatinohd" to ::Animelatinohd,
-        "SPAGNOLO: Henaojara" to ::Henaojara,
-        "SPAGNOLO: Jkanime" to ::Jkanime,
-        "SPAGNOLO: TioAnime" to ::TioAnime,
-        "SPAGNOLO: Monoschinos" to ::Monoschinos
-
-
+        "INGLESE: KickAssAnime" to ::KickAssAnime
 
     )
 }
